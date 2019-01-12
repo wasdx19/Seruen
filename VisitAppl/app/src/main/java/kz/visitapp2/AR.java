4b69cb85d0c7ec873c99debaa9ae5af81a5d866f
Nurgissa <usen.nurgisa@gmail.com>
2019-01-12 18:52:00 +0600

    nurgi17_branch

---
+++ VisitAppl/app/src/main/java/kz/visitapp2/AR.java
@@ -0,0 +1,13 @@
+package kz.visitapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar);
+    }
+}
