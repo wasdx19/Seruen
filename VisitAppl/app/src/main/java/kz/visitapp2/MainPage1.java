b94d22630173bc4373d413456105d21933c69a97
wasdx19 <vanrwei@gmail.com>
2019-01-16 21:13:19 +0600

    last changes from Wasdx

--- VisitAppl/app/src/main/java/kz/visitapp2/MainPage1.java
+++ VisitAppl/app/src/main/java/kz/visitapp2/MainPage1.java
@@ -3,12 +3,15 @@ package kz.visitapp2;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainPage1 extends Fragment {
@@ -22,20 +25,24 @@ public class MainPage1 extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getActivity().getWindow();
+            w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         super.onCreate(savedInstanceState);
         headerTV=getActivity().findViewById(R.id.headerTv);
-        //grad(headerTV);
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
         return inflater.inflate(R.layout.activity_main_page1,container,false);
     }
 
-    public void grad(TextView v) {
+    /*public void grad(TextView v) {
         Shader myShader = new LinearGradient(
                 0, 0, 0, 100,
                 Color.WHITE, Color.BLACK,
                 Shader.TileMode.CLAMP);
         v.getPaint().setShader(myShader);
-    }
+    }*/
 }
