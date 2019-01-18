ee625e07720c0cbc310efcefd6cc1832b86dcfd7
Nurgissa <usen.nurgisa@gmail.com>
2019-01-18 20:47:53 +0600

    changes 6

--- VisitAppl/app/src/main/java/kz/visitapp2/InterestsPage.java
+++ VisitAppl/app/src/main/java/kz/visitapp2/InterestsPage.java
@@ -1,13 +1,25 @@
 package kz.visitapp2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InterestsPage extends AppCompatActivity {
-
+    Button skip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interests_page);
+
+        skip = findViewById(R.id.button_ok);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InterestsPage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
