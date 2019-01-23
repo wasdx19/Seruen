9ca3f8ad35376ba9283f9ba7bc8a9173e7cba2b1
Bexultan <zh.beksultan1412@mail.ru>
2019-01-23 19:15:14 +0600

    modified front end places 1 and 2

--- VisitAppl/app/src/main/java/kz/visitapp2/PlacesPage2.java
+++ VisitAppl/app/src/main/java/kz/visitapp2/PlacesPage2.java
@@ -1,12 +1,20 @@
 package kz.visitapp2;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class PlacesPage2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places_page2);
     }
