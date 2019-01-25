49a6612f094a111d28e974a9d778751d5ae62172
Nurgissa <usen.nurgisa@gmail.com>
2019-01-25 20:00:52 +0600

    changes 11

--- VisitAppl/app/src/main/java/kz/visitapp2/GuidePageClasses/GuidePage1.java
+++ VisitAppl/app/src/main/java/kz/visitapp2/GuidePageClasses/GuidePage1.java
@@ -9,22 +9,11 @@ import android.widget.Button;
 
 import kz.visitapp2.R;
 
-//import com.larvalabs.svgandroid.SVG;
-//import com.larvalabs.svgandroid.SVGParser;
-
 public class GuidePage1 extends AppCompatActivity {
-//    TextView text1;
-//    TextView text2;
-//    TextView text3;
-//    TextView text4;
-//    TextView text5;
-//    Typeface tfc1;
-//
     Button skip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
             w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
@@ -33,20 +22,6 @@ public class GuidePage1 extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide_page1);
-
-        //-----------------------------------------------------------------------------------------------
-//        text1 = (TextView)findViewById(R.id.GP1_text_1);
-//        text2 = (TextView)findViewById(R.id.GP1_text_2);
-//        text3 = (TextView)findViewById(R.id.GP1_text_3);
-//        text5 = (TextView)findViewById(R.id.GP1_button_skip);
-//        tfc1 = Typeface.createFromAsset(getAssets(), "fonts/futura_medium.ttf");
-//
-//        text1.setTypeface(tfc1);
-//        text2.setTypeface(tfc1);
-//        text3.setTypeface(tfc1);
-//        text5.setTypeface(tfc1);
-
-        //-----------------------------------------------------------------------------------------------
 //
 //        skip=findViewById(R.id.GP1_button_skip);
 //        skip.setOnClickListener(new View.OnClickListener() {
